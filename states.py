d9384d1c899fa34b2832f25b074d3276f9f032f8
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-10 20:25:36 +0300

    Create states.py

---
+++ states.py
@@ -0,0 +1,33 @@
+import time
+
+user_button = {}
+last_request_time = {}
+
+
+async def last_time_usage(user_id):
+    current_time = time.time()
+    if user_id in last_request_time and (current_time - last_request_time[user_id]) < 2:
+        return False
+    last_request_time[user_id] = current_time
+    return True
+
+
+async def get_titul(card_count, user_id):
+    if user_id in [1130692453, 1268026433]:
+        return "Создатель"
+    if user_id in [1497833411, 6679727618, 5872877426]:
+        return "Лох"
+    if card_count > 500:
+        return "Мастер карточек"
+    elif card_count > 250:
+        return "Коллекционер"
+    elif card_count > 150:
+        return 'Эксперт карточек'
+    elif card_count > 100:
+        return 'Продвинутый коллекционер'
+    elif card_count > 50:
+        return 'Любитель Комару'
+    elif card_count > 20:
+        return 'Начинающий коллекционер'
+    else:
+        return 'Новичок'
