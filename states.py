41d4273562676fa781d0f09c33faca450038668d
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-18 01:30:36 +0300

    Update states.py

--- states.py
+++ states.py
@@ -15,9 +15,13 @@ async def last_time_usage(user_id):
 async def get_titul(card_count, user_id):
     if user_id in [1130692453, 1268026433, 6184515646]:
         return "Создатель"
-    if user_id in [1497833411, 6679727618, 5872877426]:
+    elif user_id in [1497833411, 6679727618, 5872877426]:
         return "Лох"
-    if card_count > 500:
+    elif user_id == "6678042511":
+        return "вирус"
+    elif user_id == "1829798663":
+        return "ГЕЙ"
+    elif card_count > 500:
         return "Мастер карточек"
     elif card_count > 250:
         return "Коллекционер"
