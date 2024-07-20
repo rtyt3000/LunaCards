373a4fb937044d1020d0feb0d822c4e6c96abe12
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-07-20 21:47:43 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -159,7 +159,7 @@ async def setup_router(dp, bot):
         premium_status, premium_expiration = await check_and_update_premium_status(user_id)
         premium_message = f"Премиум: активен до {premium_expiration}" if premium_status else "Премиум: не активен"
     
-        if user_id in [6184515646, 1268026433, 5493956779]:
+        if user_id in [6184515646, 1268026433, 5493956779, 1022923020]:
             dev_titul = await get_dev_titul(user_id)
             dev_titul_message = f"🪬 Dev Титул: {dev_titul}"
         else:
